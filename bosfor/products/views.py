28fb49456f132715508a3f8fe4872ec7bfd68c67
Mikhail Ermakov <1pestilent@gmail.com>
2024-04-19 12:05:08 +0500

    Создан проект джанго, переделаны шаблоны в приложение Products по принципу DRY, созданы временные контроллеры

---
+++ bosfor/products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'products/index.html')
+
+
+def products(request):
+    return render(request, 'products/products.html')
+
+def product(request):
+    return render(request, 'products/product.html')
