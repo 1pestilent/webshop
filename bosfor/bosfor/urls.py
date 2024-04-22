a166a8d56aafd45646a91ad7cee4192517dec98e
Mikhail Ermakov <1pestilent@gmail.com>
2024-04-22 18:51:06 +0500

    Полностью переработал логику сортировки

--- bosfor/bosfor/urls.py
+++ bosfor/bosfor/urls.py
@@ -10,13 +10,9 @@ urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
     path('', index, name="index"),
     path('products/', products, name="products"),
+    path('products/<int:gender_id>', products, name="gender"),
+    path('products/<int:gender_id>/<int:category_id>', products, name="gendercat"),
     path('products/product/', product, name="product"),
-    path('products/man/', all_products_for_mans, name="productforman"),
-    path('products/woman/', all_products_for_womans, name="productforwoman"),
-    path('products/kid/', all_products_for_kids, name="productforkid"),
-    path('products/man/category/<int:category_id>/', category_products_for_man, name="ctproductforman"),
-    path('products/woman/category/<int:category_id>/', category_products_for_woman, name="ctproductforwoman"),
-    path('products/kid/category/<int:category_id>/', category_products_for_kid, name="ctproductforkid"),
 ]
 
 if settings.DEBUG:
--- bosfor/products/views.py
+++ bosfor/products/views.py
@@ -4,97 +4,22 @@ from products.models import *
 def index(request):
     context = {
         'title': 'Bosfor - главная',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
+        'genders': Gender.objects.all()
     }
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, gender_id = None, category_id = None):
+    products = Product.objects.all()
+    if gender_id:
+        products = products.filter(sex_id=gender_id)  
+        if category_id:
+            products = products.filter(category_id=category_id)
+    
     context = {
         'title': 'Bosfor - каталог',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
-        'products': Product.objects.all(),
-    }
-    return render(request, 'products/products.html', context)
-
-def all_products_for_mans(request):
-    context = {
-        'title': 'Bosfor - товары для мужчин',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
-        'products': Product.objects.filter(sex__name='Мужской'),
-    }
-    return render(request, 'products/products.html', context)
-
-def all_products_for_womans(request):
-    context = {
-        'title': 'Bosfor - товары для женщин',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
-        'products': Product.objects.filter(sex__name='Женский'),
-    }
-    return render(request, 'products/products.html', context)
-
-def all_products_for_kids(request):
-    context = {
-        'title': 'Bosfor - товары для детей',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
-        'products': Product.objects.filter(sex__name='Дети'),
-    }
-    return render(request, 'products/products.html', context)
-
-def category_products_for_man(request, category_id):
-    if category_id:
-        category = get_object_or_404(Category, pk=category_id)
-        products = Product.objects.filter(category=category, sex__name='Мужской')
-    else:
-        products = Product.objects.filter(sex__name='Мужской')
-    
-    context = {
-        'title': 'Bosfor - товары для мужчин',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
-        'products': products,
-    }
-    return render(request, 'products/products.html', context)
-
-def category_products_for_woman(request, category_id):
-    if category_id:
-        category = get_object_or_404(Category, pk=category_id)
-        products = Product.objects.filter(category=category, sex__name='Женский')
-    else:
-        products = Product.objects.filter(sex__name='Женский')
-    
-    context = {
-        'title': 'Bosfor - товары для женщин',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
-        'products': products,
-    }
-    return render(request, 'products/products.html', context)
-
-def category_products_for_kid(request, category_id):
-    if category_id:
-        category = get_object_or_404(Category, pk=category_id)
-        products = Product.objects.filter(category=category, sex__name='Дети')
-    else:
-        products = Product.objects.filter(sex__name='Дети')
-    
-    context = {
-        'title': 'Bosfor - товары для детей',
-        'man_categorys': Category.objects.filter(product__sex__name='Мужской').distinct(),
-        'woman_categorys': Category.objects.filter(product__sex__name='Женский').distinct(),
-        'kid_categorys': Category.objects.filter(product__sex__name='Дети').distinct(),
+        'genders': Gender.objects.all(),
+        'categories': Category.objects.filter().distinct(),
         'products': products,
     }
     return render(request, 'products/products.html', context)
