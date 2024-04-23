4fdaf56e576a2f009791edc4df2513277bcfeebd
Mikhail Ermakov <1pestilent@gmail.com>
2024-04-23 19:22:43 +0500

    Теперь отображаются только те товары, которые имеются на складе, настроенна админ панель

--- bosfor/products/admin.py
+++ bosfor/products/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-
 from products.models import *
 
-admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Gender)
 admin.site.register(ImagesForProduct)
+
+
+class QuantityOfClothesInline(admin.TabularInline):
+    model = QuantityOfClothes
+    extra = 0
+
+class QuantityOfShoesInline(admin.TabularInline):
+    model = QuantityOfShoes
+    extra = 0
+
+class ImagesForProductInline(admin.TabularInline):
+    model = ImagesForProduct
+    extra = 1
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [QuantityOfClothesInline, QuantityOfShoesInline, ImagesForProductInline]
+    list_display = ['name','sex','category','price']
+    list_filter = ['sex','category']
+    fields = ('name', 'description',('price','category','sex'),'image_for_card')
+    search_fields = ('name',)
+    ordering = ('name', )
+
+admin.site.register(Product, ProductAdmin)
--- bosfor/products/views.py
+++ bosfor/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from products.models import *
+from django.db.models import Sum, F, Case, When
 
 def index(request):
     context = {
@@ -11,7 +12,68 @@ def index(request):
 
 
 def products(request, gender_id = None, category_id = None):
-    products = Product.objects.all()
+    products = Product.objects.annotate(
+    total_quantity=Sum(
+                Case(
+                    When(
+                        quantityofclothes__XS__gt=0, then=F('quantityofclothes__XS')
+                    ),
+                    When(
+                        quantityofclothes__S__gt=0, then=F('quantityofclothes__S')
+                    ),
+                    When(
+                        quantityofclothes__M__gt=0, then=F('quantityofclothes__M')
+                    ),
+                    When(
+                        quantityofclothes__L__gt=0, then=F('quantityofclothes__L')
+                    ),
+                    When(
+                        quantityofclothes__XL__gt=0, then=F('quantityofclothes__XL')
+                    ),
+                    default=0
+                ) +
+                Case(
+                    When(
+                        quantityofshoes__s35__gt=0, then=F('quantityofshoes__s35')
+                    ),
+                    When(
+                        quantityofshoes__s36__gt=0, then=F('quantityofshoes__s36')
+                    ),
+                    When(
+                        quantityofshoes__s37__gt=0, then=F('quantityofshoes__s37')
+                    ),
+                    When(
+                        quantityofshoes__s38__gt=0, then=F('quantityofshoes__s38')
+                    ),
+                    When(
+                        quantityofshoes__s39__gt=0, then=F('quantityofshoes__s39')
+                    ),
+                    When(
+                        quantityofshoes__s40__gt=0, then=F('quantityofshoes__s40')
+                    ),
+                    When(
+                        quantityofshoes__s41__gt=0, then=F('quantityofshoes__s41')
+                    ),
+                    When(
+                        quantityofshoes__s42__gt=0, then=F('quantityofshoes__s42')
+                    ),
+                    When(
+                        quantityofshoes__s43__gt=0, then=F('quantityofshoes__s43')
+                    ),
+                    When(
+                        quantityofshoes__s44__gt=0, then=F('quantityofshoes__s44')
+                    ),
+                    When(
+                        quantityofshoes__s45__gt=0, then=F('quantityofshoes__s45')
+                    ),
+                    When(
+                        quantityofshoes__s46__gt=0, then=F('quantityofshoes__s46')
+                    ),
+                    default=0
+                )
+            )
+        ).filter(total_quantity__gt=0)
+    
     if gender_id:
         products = products.filter(sex_id=gender_id)  
         if category_id:
@@ -27,10 +89,23 @@ def products(request, gender_id = None, category_id = None):
 
 def product(request, product_id=None):
     product = get_object_or_404(Product, id=product_id)
+    
+    try:
+        quantity_of_clothes = QuantityOfClothes.objects.get(product=product)
+    except QuantityOfClothes.DoesNotExist:
+        quantity_of_clothes = None
+    
+    if quantity_of_clothes is None:
+        quantity_of_shoes = QuantityOfShoes.objects.get(product=product)
+    else:
+        quantity_of_shoes = None
+
     context = {
         'title': product.name,
         'product': product,
         'genders': Gender.objects.all(),
         'categories': Category.objects.filter().distinct(),
+        'quantity_of_clothes': quantity_of_clothes,
+        'quantity_of_shoes': quantity_of_shoes,
     }
     return render(request, 'products/product.html',context)
